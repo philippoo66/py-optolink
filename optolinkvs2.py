39ef01ee8b98079b1dac886601b2166ed3cb2052
Phil <122479122+philippoo66@users.noreply.github.com>
2024-04-15 19:24:50 +0200

    Add files via upload

--- optolinkvs2.py
+++ optolinkvs2.py
@@ -2,6 +2,9 @@ import serial
 import sys
 import time
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Optolink VS2 / 300 Protocol, mainly virtual r/w datapoints
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 def init_vs2(ser:serial.Serial) -> bool:
 
@@ -48,12 +51,12 @@ def init_vs2(ser:serial.Serial) -> bool:
     return True
 
 
-def read_data(addr:int, rdlen:int, ser:serial.Serial) -> bytes:
+def read_datapoint(addr:int, rdlen:int, ser:serial.Serial) -> bytes:
     outbuff = bytearray(8)
     outbuff[0] = 0x41   # 0x41 Telegrammstart
     outbuff[1] = 0x05   # Len Payload, hier immer 5
-    outbuff[2] = 0x00   # 0x00 Anfrage
-    outbuff[3] = 0x01   # 0x01 Lesen
+    outbuff[2] = 0x00   # 0x00 Request Message
+    outbuff[3] = 0x01   # 0x01 Virtual_READ
     outbuff[4] = (addr >> 8) & 0xFF  # hi byte
     outbuff[5] = addr & 0xFF         # lo byte
     outbuff[6] = rdlen   # Anzahl der zu lesenden Daten-Bytes
@@ -64,54 +67,17 @@ def read_data(addr:int, rdlen:int, ser:serial.Serial) -> bytes:
     ser.write(outbuff)
     print("R tx", bbbstr(outbuff))
 
-    # for up 30x100ms serial data is read.
-    i = 0
-    state = 0
-    inbuff = []
-    while(True):
-        time.sleep(0.1)
-        inbuff += ser.read(ser.in_waiting)
-
-        if(state == 0):
-            if(len(inbuff) > 0):
-                if(inbuff[0] == 0x06): # VS2_ACK
-                    state = 1
-                elif (inbuff[0] == 0x15): # VS2_NACK
-                    # hier müsste ggf noch ein eventueller Rest des Telegrams abgewartet werden 
-                    return []
-        
-        if(state == 1):
-            if(len(inbuff) > 2):
-                if(inbuff[1] != 0x41): # STX
-                    return []
-                state = 2
-
-        if(state == 2):
-            dlen = inbuff[2]
-            if(len(inbuff) >= dlen+4):  # 0x06 + 0x41 + Len + Nutzdaten + CRC
-                print("R rx", bbbstr(inbuff))
-                crc = inbuff[dlen+3] 
-                if(crc != calc_crc(inbuff)):
-                    print("CRC Error")
-                    return []
-                if(inbuff[3] & 0x0F == 0x03):
-                    print("Error Message")
-                    return []
-                return inbuff[8:8+rdlen]
-
-        i+=1
-        if(i == 30):
-            print("Timeout")
-            return []
+    err, data = receive_response(ser)
+    return data
 
 
-def write_data(addr:int, data:bytes, ser:serial.Serial) -> bool:
+def write_datapoint(addr:int, data:bytes, ser:serial.Serial) -> bool:
     wrlen = len(data)
     outbuff = bytearray(wrlen+8)
     outbuff[0] = 0x41   # 0x41 Telegrammstart
     outbuff[1] = 5 + wrlen  # Len Payload
-    outbuff[2] = 0x00   # 0x00 Anfrage
-    outbuff[3] = 0x02   # 0x02 Schreiben
+    outbuff[2] = 0x00   # 0x00 Request Message
+    outbuff[3] = 0x02   # 0x02 Virtual_WRITE 
     outbuff[4] = (addr >> 8) & 0xFF  # hi byte
     outbuff[5] = addr & 0xFF         # lo byte
     outbuff[6] = wrlen  # Anzahl der zu schreibenden Daten-Bytes
@@ -119,15 +85,24 @@ def write_data(addr:int, data:bytes, ser:serial.Serial) -> bool:
         outbuff[7 + i] = data[i]
     outbuff[7 + wrlen] = calc_crc(outbuff)
 
+    ser.reset_input_buffer()
     ser.write(outbuff)
     print("W tx", bbbstr(outbuff))
 
-    # for up 30x100ms serial data is read.
+    err, data = receive_response(ser)
+    return (err == 0x01)
+
+
+def receive_response(ser:serial.Serial):    # -> int, bytearray: Err_Code, Data
+    # Err_Code: 01=success, 03=ErrMsg, 15=NACK, 42=STX_Err, FE=CRC_Err, FF=TimeOut (all hex)
+    # receives the V2 response to a Virtual_READ or Virtual_WRITE request
     i = 0
     state = 0
     inbuff = []
+    data = []
+    # for up 30x100ms serial data is read. (we do 300x10ms)
     while(True):
-        time.sleep(0.1)
+        time.sleep(0.01)
         inbuff += ser.read(ser.in_waiting)
 
         if(state == 0):
@@ -135,45 +110,50 @@ def write_data(addr:int, data:bytes, ser:serial.Serial) -> bool:
                 if(inbuff[0] == 0x06): # VS2_ACK
                     state = 1
                 elif (inbuff[0] == 0x15): # VS2_NACK
-                    # hier müsste ggf noch ein eventueller Rest des Telegrams abgewartet werden 
-                    return False
+                    print("NACK Error")
+                    return 0x15, []       # hier müsste ggf noch ein eventueller Rest des Telegrams abgewartet werden 
         
         if(state == 1):
             if(len(inbuff) > 2):  # Ack, STX, Len
                 if(inbuff[1] != 0x41): # STX
-                    return False
+                    print("STX Error")
+                    return 0x41, []    # hier müsste ggf noch ein eventueller Rest des Telegrams abgewartet werden
                 state = 2
 
         if(state == 2):
-            dlen = inbuff[2]
-            if(len(inbuff) >= dlen+4):  # 0x06 + 0x41 + Len + Nutzdaten + CRC
-                print("W rx", bbbstr(inbuff))
-                crc = inbuff[dlen+3] 
+            pllen = inbuff[2]
+            if(len(inbuff) >= pllen+4):  # 0x06 + 0x41 + Len + Payload + CRC
+                print("rx", bbbstr(inbuff))
+                inbuff = inbuff[0:pllen+4]  # make sure no tailing trash 
+                data = inbuff[8:-1]   # 0x06 + 0x41 + Len + FnctCode + MsgId + AddrHi + AddrLo + BlkLen (+ Data) + CRC
+                crc = inbuff[pllen+3] 
                 if(crc != calc_crc(inbuff)):
                     print("CRC Error")
-                    return False
+                    return 0xFE, data
                 if(inbuff[3] & 0x0F == 0x03):
-                    print("Error Message")
-                    return False
-                return True
-
+                    print("Error Message", bbbstr(data))
+                    return 0x03, data
+                # success
+                return 0x01, data 
+        # timout
         i+=1
-        if(i == 30):
+        if(i > 300):
             print("Timeout")
-            return False
-
+            return 0xFF, inbuff
 
 
 def calc_crc(telegram):
+    # CRC, a modulo-256 addition of bytes from Block Length and the additional bytes.
     CRCsum = 0
     telestart = 1
     teleend = telegram[1] + 1
 
-    if telegram[0] != 0x41:
+    if telegram[0] != 0x41: # STX
         telestart += 1
         teleend = telegram[2] + 2
         if (telegram[0] != 0x06) and (telegram[1] != 0x41):
-            return 0
+            print("ugly telegram", bbbstr(telegram))
+            return 0  # 1:256 that it fits nevertheless
 
     for i in range(telestart, teleend + 1):
         CRCsum += telegram[i]
@@ -191,7 +171,7 @@ def bytesval(data, div=1, signd=True):
 
 # Hauptfunktion
 def main():
-    port = '/dev/ttyUSB0' #'COM1'
+    port = "COM1"  #'/dev/ttyUSB0' #'COM1'
 
     if(len(sys.argv) > 1):
         port = sys.argv[1]
@@ -209,44 +189,44 @@ def main():
         
         # read test
         while(True):
-            buff = read_data(0x00f8, 8, ser)
+            buff = read_datapoint(0x00f8, 8, ser)
             print("0x00f8", bbbstr(buff))
             time.sleep(0.1)
 
-            buff = read_data(0x0802, 2, ser)
+            buff = read_datapoint(0x0802, 2, ser)
             print("KT", bbbstr(buff), bytesval(buff, 10))
             time.sleep(0.1)
 
-            buff = read_data(0x0804, 2, ser)
+            buff = read_datapoint(0x0804, 2, ser)
             print("WW", bbbstr(buff), bytesval(buff, 10))
             time.sleep(1)
 
 
         # write test
         if(False):
-            buff = read_data(0x6300, 1, ser)
+            buff = read_datapoint(0x6300, 1, ser)
             currval = buff
             print("Soll Ist", bbbstr(buff), bytesval(buff))
             
             time.sleep(1)
 
             data = bytes([50])
-            ret = write_data(0x6300, data, ser)
+            ret = write_datapoint(0x6300, data, ser)
             print("write succ", ret)
 
             time.sleep(2)
 
-            buff = read_data(0x6300, 1, ser)
+            buff = read_datapoint(0x6300, 1, ser)
             print("Soll neu", bbbstr(buff), bytesval(buff))
 
             time.sleep(1)
 
-            ret = write_data(0x6300, currval, ser)
+            ret = write_datapoint(0x6300, currval, ser)
             print("write back succ", ret)
 
             time.sleep(2)
 
-            buff = read_data(0x6300, 1, ser)
+            buff = read_datapoint(0x6300, 1, ser)
             print("Soll read back", bbbstr(buff), bytesval(buff))
 
     
@@ -263,4 +243,5 @@ def main():
             ser.close()
 
 
-main()
+if __name__ == "__main__":
+    main()
